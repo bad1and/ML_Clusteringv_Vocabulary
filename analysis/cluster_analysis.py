263b6ce6e9c212b36cefcf8fd1591b2ad5206749
AleX <2006akimov@gmail.com>
2026-05-10 04:48:20 +0300

    commented cluster_analysis.py

--- analysis/cluster_analysis.py
+++ analysis/cluster_analysis.py
@@ -1,12 +1,16 @@
+# получить самые важные слова в кластерах
 def get_top_words(G, clusters, top_n=5):
-    result = {}
+    result = {} # итоговое множество
 
+    # для каждого кластера
     for cid, words in clusters.items():
+
+        # сортировка важности слов
         scored = sorted(
             words,
             key=lambda w: G.degree(w, weight="weight"),
             reverse=True
         )
-        result[cid] = scored[:top_n]
+        result[cid] = scored[:top_n] # выбираем первые N важных слов
 
     return result
